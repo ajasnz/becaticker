e632d1eefcb72163c37ad4e3b3c16ad90c529dd1
ajasnz <alex@ajas.nz>
2025-10-12 10:26:50 +1300

    mapping...

--- becaticker.py
+++ becaticker.py
@@ -751,16 +751,21 @@ class ClockDisplay:
         # Physical layout:
         # [Panel4: TL] [Panel3: TR]
         # [Panel1: BL] [Panel2: BR]
-        if panel_y == 0:  # Top row (right-to-left in chain)
-            if panel_x == 0:  # Top-left -> Panel 4 (4th in chain)
-                panel_offset = 192  # Panel 4: pixels 192-255
-            else:  # Top-right -> Panel 3 (3rd in chain)
-                panel_offset = 128  # Panel 3: pixels 128-191
-        else:  # Bottom row (left-to-right in chain)
-            if panel_x == 0:  # Bottom-left -> Panel 1 (1st in chain)
-                panel_offset = 0  # Panel 1: pixels 0-63
-            else:  # Bottom-right -> Panel 2 (2nd in chain)
-                panel_offset = 64  # Panel 2: pixels 64-127
+        # Corrected mapping based on observations:
+        # Top-right (offset 128) is correct
+        # Bottom-left (offset 0) is missing -> try different offset
+        # Bottom-right shows top-left -> wrong mapping
+        # Top-left shows bottom-right -> wrong mapping
+        if panel_y == 0:  # Top row
+            if panel_x == 0:  # Top-left -> currently shows bottom-right, try bottom-left panel
+                panel_offset = 0    # Try Panel 1 (was missing from bottom-left)
+            else:  # Top-right -> this is correct
+                panel_offset = 128  # Panel 3: keep this
+        else:  # Bottom row
+            if panel_x == 0:  # Bottom-left -> currently missing, try top-left panel
+                panel_offset = 192  # Try Panel 4 (was showing at top-left)
+            else:  # Bottom-right -> currently shows top-left, try remaining panel
+                panel_offset = 64   # Try Panel 2
 
         physical_x = local_x + panel_offset
         physical_y = local_y + self.row_offset
